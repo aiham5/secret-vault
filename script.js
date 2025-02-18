3bca7fe51d6762579ab63ecdcbd327a67e8ce583
Aiham Hamoudh <aiham5hamoudh@gmail.com>
2025-02-18 11:53:10 -0600

    finished

---
+++ script.js
@@ -0,0 +1,9 @@
+// I will use mutliple types of operations to calucation the three codes for the vault, then I will use a an alert to display the code on the website.
+
+
+
+let num1 = 5 + 5; // addition to get 10
+let num2 = 5 * 8; // mutliplcation to get 40
+let num3 = 40 - 1; // subtraction to get 39
+
+alert (`You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`);
